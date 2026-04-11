8ca40b2982d6247e8f9a52a508fc4f4a5ddbf91f
Caiden-Heinrichs <cjheinrichs@students.unwsp.edu>
2026-04-10 19:45:41 -0500

    Completed Employee.py

--- Employee.py
+++ Employee.py
@@ -1,10 +1,38 @@
-# Program #4 Employee Class:
-# Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
+#Week 10, Program 4 - Employee Class
+#Caiden Heinrichs
+#04/10/26
 
-# Once you have written the class, write a program that creates three Employee objects to hold the following data.
+class Employee:
+    #Initiate attributes
+    def __init__(self, name, idNumber, department, jobTitle):
+        self.name = name
+        self.idNumber = idNumber
+        self.department = department
+        self.jobTitle = jobTitle
 
-# Name	ID Number	Department	Job Title
-# Susan Meyers	47899 	Accounting	Vice President
-# Mark Jones	39119	IT	Programmer
-# Joy Rogers	81774	Manufacturing	Engineer
-# The program should store the data in the three objects, then display the data for each employee on the screen.
+
+def main():
+    #Create employee objects
+    employee1 = Employee('Susan Meyers', '47899', 'Accounting', 'Vice President')
+    employee2 = Employee('Mark Jones', '39119', 'IT', 'Programmer')
+    employee3 = Employee('Joy Rogers', '81774', 'Manufacturing', 'Engineer')
+
+    #Print employee information
+    print(f'First employee name: {employee1.name}')
+    print(f'First employee ID: {employee1.idNumber}')
+    print(f'First employee department: {employee1.department}')
+    print(f'First employee job title: {employee1.jobTitle}', end='\n\n')
+
+    print(f'Second employee name: {employee2.name}')
+    print(f'Second employee ID: {employee2.idNumber}')
+    print(f'Second employee department: {employee2.department}')
+    print(f'Second employee job title: {employee2.jobTitle}', end='\n\n')
+
+    print(f'Third employee name: {employee3.name}')
+    print(f'Third employee ID: {employee3.idNumber}')
+    print(f'Third employee department: {employee3.department}')
+    print(f'Third employee job title: {employee3.jobTitle}')
+
+
+if __name__ == '__main__':
+    main()
